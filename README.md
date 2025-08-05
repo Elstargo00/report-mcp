0a9bb474fd9861a516cffb880affe708ead00d6e
Elstargo <tagam2707@gmail.com>
2025-08-05 15:00:20 +0700

    add readme.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# Manual
+
+1. Craft your report in report.md
+2. Define your template at templates/...
+3. Make it more vivid styles/..
+4. run "./render_report.sh" to get pdf report
---
+++ render_report.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+mkdir -p assets styles templates tmp 
+touch report.md
+
+pandoc report.md \
+--template="templates/default.html" -s -o "tmp/report.html" \
+--metadata title="Donaldson SVS1 Report" \
+--css="styles/styles.css"
+
+cp styles/styles.css tmp/styles.css
+uv run weasyprint --stylesheet "tmp/styles.css" tmp/report.html report.pdf
+
+rm -r tmp
